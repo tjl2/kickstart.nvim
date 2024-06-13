6f0fedd25ed475c83948ad165bbc5e648b1519ed
Tim Littlemore <tim@tjl2.com>
2024-06-13 22:01:14 +0100

    Colemak HJKL as Alt-NEIO
    Note that this relies on Alacritty's `option_as_alt = "OnlyLeft"` being
    set.

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -180,6 +180,12 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+--
+-- Keybinds for Colemak HJKL equivalent (Alt+N,E,I,O)
+vim.keymap.set({ 'n', 'o', 'x' }, '<M-n>', 'h', { desc = "Colemak version of 'h'", noremap = true, silent = true })
+vim.keymap.set({ 'n', 'o', 'x' }, '<M-e>', 'j', { desc = "Colemak version of 'j'", noremap = true, silent = true })
+vim.keymap.set({ 'n', 'o', 'x' }, '<M-i>', 'k', { desc = "Colemak version of 'k'", noremap = true, silent = true })
+vim.keymap.set({ 'n', 'o', 'x' }, '<M-o>', 'l', { desc = "Colemak version of 'l'", noremap = true, silent = true })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
